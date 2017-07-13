b456b6214ec59961089b49f42962f8d00446ef0b
Nishio Takuya <littlecub240@gmail.com>
2017-07-13 23:46:02 +0900

    generate uuid and url validation for site_urls

--- spec/factories/payloader_site_urls.rb
+++ spec/factories/payloader_site_urls.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :payloader_site_url, class: 'Payloader::SiteUrl' do
     site_id 1
-    uuid "MyString"
-    url "MyString"
+    uuid ""
+    url "http://localhost"
     livemode false
   end
 end
--- spec/models/payloader/site_url_spec.rb
+++ spec/models/payloader/site_url_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 module Payloader
   RSpec.describe SiteUrl, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:payloader_site) {
+      FactoryGirl.create(:payloader_site)
+    }
+
+    let(:payloader_site_url) {
+      FactoryGirl.create(:payloader_site_url, site_id: payloader_site.id)
+    }
+
+    subject { payloader_site_url }
+    it {should respond_to(:uuid)}
+    it {should respond_to(:url)}
+
+    it 'created at generate uuid' do
+      expect(payloader_site.uuid.present?).to eq true
+    end
   end
 end
